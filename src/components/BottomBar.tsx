7a2ed9025636a7d43e615f249341b1045adc5e9c
Augusto.9237 <augusto.souza8330@gmail.com>
2023-02-25 02:34:42 -0300

    ok

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,21 +1,31 @@
 import Link from "next/link"
-import { RiHome3Fill } from "react-icons/ri";
+import { RiHome3Fill, RiMenuFill, RiUserFill } from "react-icons/ri";
 
 export function BottomBar() {
     return (
         <footer className="fixed bottom-0 flex w-full h-16 bg-primaryColor-100">
-            <div className="flex flex-1 p-3 items-center">
+            <div className="flex flex-1 p-3 items-center justify-around gap-2">
+                
                 <Link href='/' className="flex flex-col items-center">
                     <button className="flex text-backgroundColor-900 text-2xl items-center">
                         <RiHome3Fill/>
                     </button>
                     <span className="text-primaryColor-300">Home</span>
                 </Link>
+
+                 
                 <Link href='/' className="flex flex-col items-center">
                     <button className="flex text-backgroundColor-900 text-2xl items-center">
-                        <RiHome3Fill/>
+                        <RiMenuFill/>
                     </button>
                     <span className="text-primaryColor-300">Home</span>
+                </Link>
+
+                <Link href='/profile' className="flex flex-col items-center">
+                    <button className="flex text-backgroundColor-900 text-2xl items-center">
+                        <RiUserFill/>
+                    </button>
+                    <span className="text-primaryColor-300">Perfil</span>
                 </Link>
             </div>
         </footer>
