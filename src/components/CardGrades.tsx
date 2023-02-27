78f667fee0e91395bd8a298fa162479e21f6a05b
Augusto.9237 <augusto.souza8330@gmail.com>
2023-02-27 02:46:33 -0300

    ok

--- src/components/CardGrades.tsx
+++ src/components/CardGrades.tsx
@@ -8,7 +8,7 @@ export function CardGrades() {
                     <span>1ª</span>
                     <h1 className="text-xl font-semibold">900</h1>
                 </div>
-                <div className="w-1 h-full bg-textColor-200"/>
+                <div className="w-1 h-full bg-textColor-200 rounded-sm"/>
 
                 <div className="flex flex-1 flex-col items-center">
                     <span>2ª</span>
