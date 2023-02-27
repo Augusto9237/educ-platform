849ac2d92cb5a4d44546e9ee612cb4d94348fa40
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-02-27 14:06:31 +0000

    cards upadate

--- src/components/CardAssessment.tsx
+++ src/components/CardAssessment.tsx
@@ -6,12 +6,21 @@ interface ContainerProps {
 }
 export function CardAssessment() {
     return (
-        <div className="relative flex flex-1 flex-col p-3 justify-center gap-1.5 w-full rounded-xl shadow-lg  bg-gradient-to-r from-backgroundColor-500/80 to-backgroundColor-500/30">
-            <h1 className="text-textColor-100 text-3xl font-bold mb-3">90%</h1>
-            <span className="flex items-center gap-2 text-textColor-100"><RiCheckboxFill color="#56D551" />900 sua média</span>
-            <span className="flex items-center gap-2 text-textColor-100"><RiCheckboxIndeterminateFill color="#E74B67" />800 média dos demais</span>
-            <div className="absolute right-3 text-7xl text-textColor-100 opacity-40">
-                <RiBarChartBoxFill/>
+        <div className="relative flex flex-1 flex-col p-3 pl-6 justify-center gap-3 w-full rounded-xl shadow-lg  bg-backgroundColor-100 overflow-hidden">
+            <div className="absolute bg-backgroundColor-500/90 w-3 h-full left-0" />
+            <div className="flex flex-row gap-2">
+                <div className="flex items-center text-4xl rounded-full p-3 text-backgroundColor-500/90 bg-backgroundColor-400/40">
+                    <RiBarChartBoxFill />
+                </div>
+                <div>
+                    <h1 className="text-textSecondaryColor-600 text-3xl font-bold">90%</h1>
+                    <span className="text-textColor-300">Janeiro</span>
+                </div>
+            </div>
+
+            <div className="flex justify-between">
+                <span className="flex items-center gap-2 text-textSecondaryColor-400 bg-textSecondaryColor-300/20 rounded p-1"><RiCheckboxFill />900 sua média</span>
+                <span className="flex items-center gap-2 text-textSecondaryColor-200 bg-textSecondaryColor-200/20  rounded p-1"><RiCheckboxIndeterminateFill/>1000 sua meta</span>
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
       backgroundColor: {
         100: "#F8F8F8",
         300: "#E1E1E6",
+        400: "#3668ff",
         500: "#222BF3",
         900: "#140169",
       },
@@ -43,6 +44,7 @@ module.exports = {
       textSecondaryColor: {
         200: "#E74B67",
         300: "#56D551",
+        400: "#20c71a",
         600: "#090030",
       },
     },
