c225020de3c09e27842211d65cc21e25346b161f
Augusto.9237 <augusto.souza8330@gmail.com>
2023-02-26 22:58:16 -0300

    ok

--- src/pages/frequency.tsx
+++ src/pages/frequency.tsx
@@ -1,42 +1,42 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import { useState } from 'react';
 
 import { BottomBar } from "../components/BottomBar";
 import { Calendar } from '../components/Calendar';
 import { CardMonth } from '../components/CardMonth';
 import { Container } from "../components/Container";
 import { Header } from "../components/Header";
-import { SummaryTable } from "../components/SumaryTable";
 
 export default function Frequency() {
-
+    const [month, setMonth] = useState(0)
     return (
         <>
             <Header titleRoutes='Frequência' />
             <Container>
                 <section className="flex flex-col flex-1 gap-6 justify-start max-sm:pb-14 ">
                     <Dialog.Root>
-                        <Dialog.Trigger>
-                            <CardMonth month="Janeiro"/>
+                        <Dialog.Trigger onClick={() => setMonth(1)}>
+                            <CardMonth month="Janeiro" />
                         </Dialog.Trigger>
-                        <Dialog.Trigger>
-                            <CardMonth month="Fevereiro"/>
+                        <Dialog.Trigger onClick={() => setMonth(2)}>
+                            <CardMonth month="Fevereiro" />
                         </Dialog.Trigger>
-                        <Dialog.Trigger>
-                            <CardMonth month="Março"/>
+                        <Dialog.Trigger onClick={() => setMonth(3)}>
+                            <CardMonth month="Março" />
                         </Dialog.Trigger>
-                        <Dialog.Trigger>
-                            <CardMonth month="Abril"/>
+                        <Dialog.Trigger onClick={() => setMonth(4)}>
+                            <CardMonth month="Abril" />
                         </Dialog.Trigger>
-                        <Dialog.Trigger>
-                            <CardMonth month="Maio"/>
+                        <Dialog.Trigger onClick={() => setMonth(5)}>
+                            <CardMonth month="Maio" />
                         </Dialog.Trigger>
                         <Dialog.Portal>
-                            <Dialog.Overlay className='w-screen h-sreen bg-textColor-900/80 fixed inset-0'>
-                                <Dialog.Content className='absolute p-4 bg-backgroundColor-100 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-hidden'>
+                            <Dialog.Overlay className='w-screen h-sreen bg-textColor-900/80 fixed inset-0 backdrop-blur-md'>
+                                <Dialog.Content className='absolute p-4 bg-backgroundColor-100 rounded-2xl max-sm:w-11/12 w-full  max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-hidden'>
                                     <Dialog.Close className='absolute right-4 top-4 text-textColor-700'>
                                         X
                                     </Dialog.Close>
-                                    <Calendar month={(6) - 1} year={2023} />
+                                    <Calendar month={month - 1} year={2023} />
                                 </Dialog.Content>
                             </Dialog.Overlay>
                         </Dialog.Portal>
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -7,17 +7,27 @@ export default function Profile() {
     return (
         <>
             <Container>
-                <div className="flex max-sm:flex-col w-full max-sm:">
-                    <div className="flex flex-1 flex-col items-center">
-                        <StudentAvatar width="80px" height="80px"/>
-                        <strong className="text-textColor-500 text-2xl">Benjamin Tayson</strong>
+                <div className="flex flex-col flex-1 w-full  max-sm:justify-center max-sm:pb-14">
+                    <div className="flex flex-col items-center h-80 justify-center gap-4">
+                        <StudentAvatar width="100px" height="100px" />
+                        <strong className="text-textSecondaryColor-600 text-2xl">Aluno Teste</strong>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <span>Aluno há 6 meses</span>
-                    </div>
-                    <section className="flex flex-1 bg-backgroundColor-300 rounded-t-3xl">
 
-                    </section>
+                    <div className="flex flex-col bg-backgroundColor-100 rounded-xl p-3">
+                        <h1 className="mx-auto text-lg font-semibold">Informações do perfil</h1>
+                        <div className="flex flex-col p-2">
+                            <span className="text-textColor-600">Nome Completo</span>
+                            <span className="text-lg font-semibold">Aluno teste</span>
+                        </div>
+                        <div className="flex flex-col p-2">
+                            <span className="text-textColor-600">E-mail</span>
+                            <span className="text-lg font-semibold">aluno@test.com</span>
+                        </div>
+                        <div className="flex flex-col p-2">
+                            <span className="text-textColor-600">Telefone</span>
+                            <span className="text-lg font-semibold">(91) 999999</span>
+                        </div>
+                    </div>
                 </div>
             </Container>
             <BottomBar />
