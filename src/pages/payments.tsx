18c563d341258741d3ffa05c36e0b94e48edbcec
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-03-01 16:20:02 +0000

    ok

--- src/pages/payments.tsx
+++ src/pages/payments.tsx
@@ -1,5 +1,5 @@
 
-import { RiCheckboxFill } from "react-icons/ri";
+import { RiCheckboxFill, RiCheckboxIndeterminateFill } from "react-icons/ri";
 import { BottomBar } from "../components/BottomBar";
 import { Container } from "../components/Container";
 import { Header } from "../components/Header";
@@ -12,16 +12,23 @@ export default function Frequency() {
             <Container>
                 <section className="flex flex-col gap-2 flex-1 p-3 justify-start rounded-xl text-textSecondaryColor-600 bg-backgroundColor-100">
                     <h1 className="mx-auto text-lg font-semibold">Mensalidades</h1>
-                    <div>
+                    <div className="flex flex-col gap-2">
                         <div className="grid grid-cols-3">
                             <span className="flex justify-center">Mês</span>
                             <span className="flex justify-center">Valor</span>
                             <span className="flex justify-center">status</span>
                         </div>
-                        <div className="grid grid-cols-3">
+                        <div className="relative grid grid-cols-3 pb-2">
                             <span className="flex justify-center">Janeiro</span>
                             <span className="flex justify-center">R$ 150,00</span>
                             <span className="flex items-center justify-center max-sm:flex-1 gap-2 text-textSecondaryColor-400 bg-textSecondaryColor-300/20 rounded "><RiCheckboxFill />Pago</span>
+                            <div className="absolute bottom-0 h-[1px] w-full bg-textColor-200" />
+                        </div>
+                        <div className="relative grid grid-cols-3 pb-2">
+                            <span className="flex justify-center">Feveiro</span>
+                            <span className="flex justify-center">R$ 150,00</span>
+                            <span className="flex items-center justify-center max-sm:flex-1 gap-2 text-textSecondaryColor-200 bg-textSecondaryColor-200/20  rounded"><RiCheckboxIndeterminateFill />Atrasado</span>
+                            <div className="absolute bottom-0 h-[1px] w-full bg-textColor-200" />
                         </div>
                     </div>
                 </section>
