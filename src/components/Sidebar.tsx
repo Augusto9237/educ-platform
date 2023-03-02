b5950e0e614661cbf6bb39c38f504861afa8cbb3
Augusto.9237 <augusto.souza8330@gmail.com>
2023-03-02 01:40:42 -0300

    ok

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,10 +4,10 @@ import { RiBarChartBoxFill, RiCalendarCheckFill, RiHome3Fill, RiMoneyDollarCircl
 
 export function Sidebar() {
     return (
-        <section className="group max-lg:hidden flex  fixed left-0 w-20 py-4 rounded-sm h-screen bg-backgroundColor-100 shadow-lg hover:w-52 delay-80 duration-500">
-            <div className="flex flex-col flex-1 gap-8 hover:pl-7">
+        <section className="group max-lg:hidden flex  fixed left-0 w-20 rounded-sm h-screen bg-backgroundColor-100 shadow-lg hover:w-52 delay-80 duration-500">
+            <div className="flex flex-col py-4 flex-1 gap-8 hover:pl-7">
              <Link href='/home' className="flex flex-col items-center text-textSecondaryColor-600 hover:text-backgroundColor-500 group-hover:items-start">
-                    <button className="flex text-2xl items-center gap-2">
+                    <button className="flex flex-row text-2xl items-center gap-2">
                         <RiHome3Fill /> <span className="hidden text-xl group-hover:flex delay-200 duration-600">Home</span>
                     </button>
                 </Link>
