ee682ed824c2ae4e6ee05d3ed4c6a48ca5834070
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-03-03 18:36:53 +0000

    sidebar ok

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { RiBarChartBoxFill, RiCalendarCheckFill, RiHome3Fill, RiLogoutBoxRFill, 
 
 export function Sidebar() {
     return (
-        <section className="group max-lg:hidden flex  fixed left-0 w-20 rounded-sm h-screen bg-backgroundColor-100 shadow-lg hover:w-52 delay-80 duration-500">
+        <section className="group z-10 max-lg:hidden flex  fixed left-0 w-20 rounded-sm h-screen bg-backgroundColor-100 shadow-lg hover:w-52 delay-80 duration-500">
             <div className="flex flex-col py-4 flex-1 gap-8">
                 <Link href='/home' className="flex flex-col  text-textSecondaryColor-600 hover:text-backgroundColor-500 group-hover:items-start pl-7">
                     <button className="flex flex-row text-2xl items-center gap-2">
@@ -30,8 +30,8 @@ export function Sidebar() {
                     </button>
                 </Link>
             </div>
-            <div className="fixed bottom-10 pl-7">
-                <Link href='/' className="flex flex-col items-center  text-buttonColor-500 hover:text-buttonColor-600 group-hover:items-start">
+            <div className="fixed bottom-8 pl-7">
+                <Link href='/' className="flex flex-col items-center  text-textSecondaryColor-200 group-hover:items-start">
                     <button className="flex  text-2xl items-center gap-2">
                     <RiLogoutBoxRFill/><span className="hidden  leading-none text-xl group-hover:flex delay-150 duration-600">Sair</span>
                     </button>
