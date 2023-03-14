32f426776f7c92d13678047d8a37aa1ab26b65d9
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-03-14 13:14:31 +0000

    home update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,3 @@
-
 import { BottomBar } from "../components/BottomBar";
 import { CardAssessment } from "../components/CardAssessment";
 import { CardFrequency } from "../components/CardFrequency";
@@ -13,18 +12,18 @@ export default function Home() {
             <Header titleRoutes='Home' />
             <Sidebar/>
             <Container>
-                <section className="w-full max-sm:flex max-sm:flex-1 max-sm:flex-col md:grid grid-cols-3 gap-6 max-sm:justify-start  max-sm:pb-14 ">
-                    <div className="flex flex-col gap-4">
+                <section className="w-full gap-4 max-sm:flex max-sm:flex-1 max-sm:flex-col md:grid  grid-cols-3  max-sm:justify-start  max-sm:pb-14 ">
+                    <div className="flex flex-col gap-2">
                         <h1 className="text-lg font-bold">Avaliações</h1>
                         <CardAssessment />
                     </div>
 
-                    <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-2">
                         <h1 className="text-lg font-bold">Frequência</h1>
                         <CardFrequency />
                     </div>
 
-                    <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-2">
                         <h1 className="text-lg font-bold">Pagamentos</h1>
                         <CardPayments/>
                     </div>
