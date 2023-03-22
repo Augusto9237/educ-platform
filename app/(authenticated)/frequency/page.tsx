a7159f3f55193bd8afe1bfbb051349056dff1a9e
Augusto9237 <augusto.souza8330@gmail.com>
2023-03-22 02:17:04 -0300

    ok

--- app/(authenticated)/frequency/page.tsx
+++ app/(authenticated)/frequency/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import * as Dialog from '@radix-ui/react-dialog';
-import { Calendar } from 'app/components/Calendar';
-import { CalendarTest } from 'app/components/CalendarTest';
+import { CalendarEvent } from 'app/components/Calendar';
 import { CardMonth } from 'app/components/CardMonth';
 import { Spinner } from 'app/components/Spinner';
 import { GlobalContext } from 'app/context/GlobalContext';
@@ -84,14 +83,14 @@ export default function Frequency() {
                         )}
                         <Dialog.Portal>
                             <Dialog.Overlay className='w-screen z-20 h-sreen bg-textColor-900/80 fixed inset-0 backdrop-blur-md'>
-                                <Dialog.Content className='absolute p-4 bg-backgroundColor-100 rounded-2xl max-sm:w-11/12 w-full  max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-hidden'>
+                                <Dialog.Content className='absolute p-3 bg-backgroundColor-100 rounded-2xl max-sm:w-11/12 w-full  max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-hidden'>
                                     <Dialog.Close className='absolute right-4 top-4 text-textColor-700'>
                                         <strong className='text-textColor-300'>X</strong>
                                     </Dialog.Close>
                                     {
                                         frequencyGroup.map(({ frequencies, month }, i) =>
                                         (
-                                            <CalendarTest key={i} month={month - 1} year={2023} frequencies={frequencies} />
+                                            <CalendarEvent key={i}  frequencies={frequencies} />
                                         )
                                         )
                                     }
--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -1,7 +1,10 @@
 "use client";
-import { useState } from 'react';
-import { RiCheckboxFill, RiCheckboxIndeterminateFill } from 'react-icons/ri';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid'
 import clsx from 'clsx';
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+import { RiCheckboxFill } from 'react-icons/ri';
 
 interface Frequency {
   __typename?: 'Frequency';
@@ -20,83 +23,48 @@ interface Frequency {
 }
 
 type CalendarProps = {
-  month: number;
-  year: number;
   frequencies: Frequency[];
 };
 
-type SelectedDate = Date | undefined;
+type events = {
+  title: string;
+  date: string;
+}[]
 
-export function Calendar({ month, year, frequencies }: CalendarProps) {
-  const daysInMonth = new Date(year, month + 1, 0).getDate();
-  const firstDayOfMonth = new Date(year, month, 1).getDay();
-
-  const days: Date[] = [];
-  for (let i = 1; i <= daysInMonth; i++) {
-    days.push(new Date(year, month, i));
-  }
-
-  const renderDaysOfWeek = () => {
-    const daysOfWeek = ['Dom', 'Seg', 'Ter', 'Quar', 'Qui', 'Sex', 'Sab'];
-    return (
-      <div className="grid grid-cols-7 text-center">
-        {daysOfWeek.map((day) => (
-          <div key={day} className="text-gray-500">
-            {day}
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  const renderDaysOfMonth = () => {
-    const daysToRender = Array(firstDayOfMonth).fill(null).map((_, index) => (
-      <div key={`empty-${index}`} className="h-6 w-6"></div>
-    ));
-    daysToRender.push(
-      ...days.map((day) => {
-        const foundFrequency = frequencies.find(frequency => {
-          const frequencyDate = new Date(frequency.createdAt);
-          return frequencyDate.getFullYear() === year && frequencyDate.getMonth() === month && frequencyDate.getDate() === day.getDate();
-        });
-
-        const presences = foundFrequency?.subscribes.filter(subscribe => subscribe.prensente);
-        const absences = foundFrequency?.subscribes.filter(subscribe => !subscribe.prensente);
-   
-        return (
-          <div
-            key={day.toString()}
-            className={clsx(
-              'h-6 w-6 flex items-center justify-center cursor-pointer rounded-full p-1',
-              {
-                'bg-textSecondaryColor-300/95 text-textSecondaryColor-600': foundFrequency && day.getDate() === new Date(foundFrequency.createdAt).getDate() && foundFrequency?.subscribes.filter(subscribe => subscribe.prensente),
-              }
-            )}
-          >
-            {day.getDate()}
-          </div>
-        );
+export function CalendarEvent({ frequencies }: CalendarProps) {
+  const [event, setEvent] = useState<events>([])
+  useEffect(() => {
+    if (frequencies) {
+      const eventFrequency = frequencies.map((frequency) => {
+        return { title: frequency.subscribes[0].prensente ? '✅' : '⛔', date: dayjs(frequency.createdAt).format('YYYY-MM-DD'), backgroundColor: frequency.subscribes[0].prensente ? "#8ce889" : "#f27a90", borderColor: 'transparent', };
       })
-    );
-    return <div className="grid grid-cols-7 gap-2 ml-4 -mr-1">{daysToRender}</div>;
-  };
+      setEvent(eventFrequency);
+    }
+  }, [frequencies])
 
   return (
-    <div>
-      <h1 className="capitalize text-lg font-bold px-2 mb-2 ">
-        {new Date(year, month).toLocaleDateString('pt-BR', {
-          month: 'long',
-        })}
-      </h1>
-      {renderDaysOfWeek()}
-      {renderDaysOfMonth()}
-
-      <div className="flex gap-4 mt-8">
-        <span className="flex items-center justify-center flex-1 gap-2 text-textSecondaryColor-400 bg-textSecondaryColor-300/20 rounded p-1"><RiCheckboxFill />2 presenças</span>
-        <span className="flex items-center justify-center flex-1 gap-2 text-textSecondaryColor-200 bg-textSecondaryColor-200/20  rounded p-1"><RiCheckboxIndeterminateFill />2 faltas</span>
-      </div>
-    </div>
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      locale="br"
+      headerToolbar={{
+        left: "",
+        center: "title",
+        right: "",
+      }}
+      firstDay={1}
+      height={425}
+      initialDate={frequencies[0].createdAt}
+      showNonCurrentDates={false}
+      events={event}
+    />
   );
 };
+
+
+
+
+
+
 
 
