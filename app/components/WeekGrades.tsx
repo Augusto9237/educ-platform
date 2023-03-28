e18b6159810d5c32d43626b2a544675f33e567df
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-03-28 17:42:36 +0000

    weekGrades ok: add media total grades

--- app/components/WeekGrades.tsx
+++ app/components/WeekGrades.tsx
@@ -1,10 +1,32 @@
 import { GradesProps } from "app/(authenticated)/assessments/page";
+import { useEffect, useState } from "react";
 
 interface WeekGradesProps {
     grades: GradesProps[];
 }
 
 export function WeekGrades({ grades }: WeekGradesProps) {
+    const [totalMedia, setTotalMedia] = useState(0)
+
+    useEffect(() => {
+        if (grades) {
+            const sumAndCount = grades.reduce(
+                (acc, curr) => {
+                    Object.values(curr).forEach((value) => {
+                        if (typeof value === "number") {
+                            acc.sum += value;
+                            acc.count++;
+                        }
+                    });
+                    return acc;
+                },
+                { sum: 0, count: 0 }
+            );
+
+            setTotalMedia(sumAndCount.sum / sumAndCount.count);
+        }
+    }, [grades])
+
     return (
         <div className="flex flex-col gap-2 text-textSecondaryColor-600">
             <header><h1 className="text-lg font-bold">Avaliação semanal</h1></header>
@@ -27,7 +49,7 @@ export function WeekGrades({ grades }: WeekGradesProps) {
             ))}
 
             <footer className="flex">
-                <h1 className="mx-auto text-lg font-bold">Média mensal: 200 pts</h1>
+                <h1 className="mx-auto text-lg font-bold">{`Média mensal: ${totalMedia} pts`}</h1>
             </footer>
         </div>
     )
