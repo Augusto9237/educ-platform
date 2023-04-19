cf48ed14ce762698b462293cf92005fe1362ce01
Augusto9237 <augusto.souza8330@gmail.com>
2023-04-19 00:02:12 -0300

    ok

--- src/components/ButtonLogin.tsx
+++ src/components/ButtonLogin.tsx
@@ -1,11 +1,12 @@
 'use client'
-import {  signIn } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { AiFillGoogleCircle } from "react-icons/ai";
- 
+import { FcGoogle } from "react-icons/fc";
+
 export function ButtonLogin() {
-    return (
-        <button onClick={() => signIn('google')} className="flex w-full justify-center items-center rounded-2xl py-5 bg-buttonColor-500 text-textSecondaryColor-600 hover:bg-buttonColor-600">
-        <strong className="mr-1">Entrar com o </strong><AiFillGoogleCircle size={20} />
-      </button >
-    )
+  return (
+    <button onClick={() => signIn('google')} className="flex w-full justify-center items-center rounded-2xl py-5 bg-buttonColor-500 text-textSecondaryColor-600 hover:bg-buttonColor-600">
+      <strong className="mr-1">Entrar com o </strong><div className="p-1 bg-backgroundColor-100 rounded-full"><FcGoogle size={20} /></div>
+    </button >
+  )
 }
