7b7dce5635449ee689050fe3c910fc4dc5de9ac8
Augusto9237 <augusto.souza8330@gmail.com>
2023-04-21 17:20:26 -0300

    button login update

--- src/components/ButtonLogin.tsx
+++ src/components/ButtonLogin.tsx
@@ -4,8 +4,9 @@ import { FcGoogle } from "react-icons/fc";
 
 export function ButtonLogin() {
   return (
-    <button onClick={() => signIn('google')} className="flex w-full justify-center items-center rounded-2xl py-5 bg-buttonColor-500 text-textSecondaryColor-600 hover:bg-buttonColor-600">
-      <strong className="mr-1">Entrar com o </strong><div className="p-1 bg-backgroundColor-100 rounded-full"><FcGoogle size={20} /></div>
+    <button onClick={() => signIn('google')} className="flex w-full justify-center items-center rounded-2xl py-5 gap-2 bg-buttonColor-500 text-textSecondaryColor-600 hover:bg-buttonColor-600">
+      <div className="p-1 bg-backgroundColor-100 rounded-full"><FcGoogle size={20} /></div>
+      <h1 className="text-lg font-semibold">Entrar com o Google</h1>
     </button >
   )
 }
