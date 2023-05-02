5c0cfa33a2fc8262c50471331ae39e11dd9db113
Augusto9237 <augusto.souza8330@gmail.com>
2023-05-02 01:26:45 -0300

    finances in progess

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -2,23 +2,31 @@
 
 import { CardBody } from "@/components/components/CardBody";
 import { useContext } from "react";
+import { FaUserGraduate } from "react-icons/fa";
 import { HiUsers } from "react-icons/hi";
 import { AdminContext } from "../../context/AdminContext";
 
 export default function Dashboard() {
-    const { subscribers, loadingUser } = useContext(AdminContext);
+    const { subscribers, classes, loadingUser } = useContext(AdminContext);
 
     return (
         <>
-            <section className="w-full gap-4 max-sm:flex max-sm:flex-1 max-sm:flex-col md:grid  grid-cols-3  max-sm:justify-start  max-sm:pb-14 ">
+            <main className="w-full gap-4 max-sm:flex max-sm:flex-1 max-sm:flex-col md:grid  grid-cols-3  max-sm:justify-start  max-sm:pb-14 ">
                 <CardBody>
                     <div className="flex items-center gap-2">
-                        <HiUsers />
+                        <FaUserGraduate />
                         <span>Alunos</span>
                     </div>
                     <h1 className="text-textSecondaryColor-600 text-2xl font-bold">{subscribers?.subscribers.length}</h1>
                 </CardBody>
-            </section>
+                <CardBody>
+                    <div className="flex items-center gap-2">
+                        <HiUsers />
+                        <span>Turmas</span>
+                    </div>
+                    <h1 className="text-textSecondaryColor-600 text-2xl font-bold">{classes?.classes.length}</h1>
+                </CardBody>
+            </main>
 
         </>
     )
